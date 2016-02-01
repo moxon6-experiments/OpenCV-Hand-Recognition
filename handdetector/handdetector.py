2113ff24c101b55f791e25d8373b1a6b72fb1250
Martin <martin.moxon@dur.ac.uk>
2016-02-01 02:00:50 +0000

    Fixed Palm Detection Bug

--- handdetector/handdetector.py
+++ handdetector/handdetector.py
@@ -7,29 +7,56 @@ from .drawables import Hand, BlankDrawable
 
 class HandDetector:
     def __init__(self):
-        self.background_remover = NotImplemented
-        self.background_training = NotImplementedError
-        self.hand = NotImplemented
-        self._start_training()
+        self.background_remover = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+        self.background_remover.setNMixtures(3)
+        self.background_training = 50
+        self.hand = BlankDrawable()
 
     def _train(self, frame):
+        """
+        Trains background remover during training mode:
+            Training area should be empty during this time
+        Extracts foreground of frame after training
+
+        Args:
+            frame (ndarray) : Current frame to train
+
+        Returns:
+            ndarray: Returns extracted foreground
+        """
         self.draw_out = np.zeros_like(frame)
         if self.background_training:
-            fore = self.background_remover.apply(frame)
+            fore = self.background_remover.apply(frame, learningRate=1)
             self.background_training -= 1
         else:
             fore = self.background_remover.apply(frame, learningRate=0)
             fore = cv2.GaussianBlur(fore, (3, 3), 3)
             fore = cv2.erode(fore, np.zeros((3, 3)))
+            fore = cv2.dilate(fore, np.zeros((3, 3)))
+            fore = cv2.dilate(fore, np.zeros((3, 3)))
             ret, fore = cv2.threshold(fore, 127, 255, cv2.THRESH_BINARY)
         return fore
 
     def _start_training(self):
+        """
+        Re-initialises training mode after program start
+        """
         self.background_remover = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
         self.background_remover.setNMixtures(3)
         self.background_training = 50
 
     def _process_contour(self, fore, contour):
+        """
+        Extracts a set of convexity defects from a contour
+
+        Args:
+            fore (ndarray): Extracted Foreground Image
+            contour
+        Returns:
+            Hand : Processes defects into a Drawable Hand if one exists
+            BlankDrawable : Blank Drawable
+
+        """
         hull = cv2.convexHull(contour, returnPoints=False)
         if len(hull):
             defects = cv2.convexityDefects(contour, hull)
@@ -82,18 +109,26 @@ class HandDetector:
 
     @staticmethod
     def _get_palm_circle(contour, fore):
+
         dist_max = np.zeros((fore.shape[0], fore.shape[1]))
+
+        cv2.imshow("WINDOW1", fore)
+
         for y in range(0, fore.shape[0], 4):
             for x in range(0, fore.shape[1], 4):
-                if fore[y][x]:
-                    dist_max[y, x] = cv2.pointPolygonTest(contour, (y, x), True)
+                if fore[y, x]:
+                    dist_max[y, x] = cv2.pointPolygonTest(contour, (x, y), True)
+
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(dist_max)
-        return (max_loc[1], max_loc[0]), max_val
+        return max_loc, max_val
 
     def get_hand(self, frame):
         fore = self._train(frame)
         processed_contours = self._get_contours(fore)
+
         if len(processed_contours) == 1:
+            fore.fill(0)
+            cv2.drawContours(fore, [processed_contours[0]], -1, (255, 0, 0), -1)
             hand = self._process_contour(fore, processed_contours[0])
             return hand
         return BlankDrawable()
